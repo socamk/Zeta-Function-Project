6b62266e530fe4d2b64e9a9239dc9cb7a36b9757
socamk <143959972+socamk@users.noreply.github.com>
2024-05-31 18:03:52 -0400

    added comments and finished implementing options

--- verification.py
+++ verification.py
@@ -73,24 +73,24 @@ def verify_RH_list(zeros, heights, value, maximum, tail):
     t0_contribution = value(1, t0)       #find the amount that t0 adds to the sum
     sum = 0         #initialize the sum
     results = []        #initialize an empty list to hold the results
-    if tail:
+    if tail:        #if including tail sum values, find the value of the sum over all zeros given
         total_sum = full_sum(zeros, value)
     for zero in zeros:      #loop through all the zeros given
         next_term = 2 * value(0.5, zero)        #find how much the current zero will add to the sum
         sum = sum + next_term           #add the amount to the sum
         if (sum + t0_contribution*2) > maximum:       #see if the sum plus the contribution from t0 exceeds the maximum
-            result = [t0, index + 1]     #if so, RH is verified, add height and number of zeros to the results
-            if tail:
+            result = [t0, index + 1]     #if so, RH is verified, record height and number of zeros
+            if tail:        #if including tail sum values, find the value by subtracting the current sum from the total sum
                 tail_sum = total_sum - sum
-                result.append(tail_sum)
-            results.append(result)
+                result.append(tail_sum)     #add the tail sum to the result list
+            results.append(result)      #add all the results to the results list
             t0 = next(heights)              #set t0 to the next height in the list
             t0_contribution = value(1.0, t0)       #find contribution of the next height
         index += 1      #increment the index at the end of each loop
     return results      #when entire loop is finished, return the results
 
 
-def verify_RH_interval(zeros, start, step, value, maximum):
+def verify_RH_interval(zeros, start, step, value, maximum, tail):
     '''
     Same as above, but this function evaluates at regular intervals instead of a list.
     
@@ -108,11 +108,17 @@ def verify_RH_interval(zeros, start, step, value, maximum):
     t0_contribution = value(1.0, t0)       #find the amount that t0 adds to the sum
     sum = 0         #initialize the sum
     results = []        #initialize an empty list to hold the results
+    if tail:        #if including tail sum values, find the value of the sum over all zeros given
+        total_sum = full_sum(zeros, value)
     for zero in zeros:      #loop through all the zeros given
         next_term = 2 * value(0.5, zero)        #find how much the current zero will add to the sum
         sum = sum + next_term           #add the amount to the sum
         if (sum + t0_contribution) > maximum:       #see if the sum plus the contribution from t0 exceeds the maximum
-            results.append([t0, index + 1])     #if so, RH is verified, add height and number of zeros to the results
+            result = [t0, index + 1]     #if so, RH is verified, record height and number of zeros
+            if tail:        #if including tail sum values, find the value by subtracting the current sum from the total sum
+                tail_sum = total_sum - sum
+                result.append(tail_sum)     #add the tail sum to the result list
+            results.append(result)      #add all the results to the results list
             t0 += step      #increment t0 by the amount given
             t0_contribution = value(1.0, t0)       #find contribution of the new height
         index += 1      #increment the index at the end of each loop
@@ -122,45 +128,56 @@ def verify_RH_interval(zeros, start, step, value, maximum):
 
 
 def main():
+    #make ArgumentParser object
     parser = argparse.ArgumentParser(description='Count the number of zeros needed to verify the Riemann Hypothesis underneath a series of heights.')
+    #create all of the options that the program can use
     parser.add_argument('-i', '--interval', action='store', nargs=2, type=float, help='interval of heights to use when verifying the RH', metavar=('START', 'STEP'))
     parser.add_argument('-z', '--zeros', action='store', type=int, help='maximum number of zeros to use')
     parser.add_argument('-m', '--method', action='store', choices=[1], default=1, help='method to use for verification. Enter 1 for 1/rho, 2 for 1/rho^2, etc.', type=int)
     parser.add_argument('-p', '--precision', action='store', choices=[2, 3, 4], nargs='+', help='precisions to use for the infinite sum', default=[2,3], type=int)
     parser.add_argument('-t', '--tail', action='store_true', help='include the sum of the tail in the results')
+    #process all of the options given in the command line arguments
     args = parser.parse_args()
     #zeros.txt contains the imaginary part of first 100,000 zeros of the Riemann zeta function
     #zeros.txt was sourced from lmfdb.org and information about these zeros can be found at
     #lmfdb.org/zeros/zeta
     all_zeros = read_zeros("zeros.txt")
+    #determine number of zeros to use in the verifcation function
     if args.zeros == None:
         zeros = all_zeros
     else:
         zeros = all_zeros[:args.zeros]
+    #determine method to use in the verifcation function (only one option right now)
     if args.method == 1:
         method = inverse_rho
         method_string = "1/rho"
+    #the values of different methods to different precisions which can be used in the verification function
+    #only one sublist because only one method exists at the moment
+    #3 and 4 significant figure values are the same because they round tp the same amount
     all_sums = [[0.024, 0.0231, 0.02310]]
+    #determine which sums are going to be used based on method and precision given
     sums = []
     for choice in args.precision:
         sums.append(all_sums[args.method - 1][choice - 2])
+    #for each sum, verify using the interval if given, the list of zeros if not
     for sum in sums:
         if args.interval == None:
             results = verify_RH_list(zeros, zeros, method, sum, args.tail)
         else:
-            results = verify_RH_interval(zeros, args.interval[0], args.interval[1], method, sum)
+            results = verify_RH_interval(zeros, args.interval[0], args.interval[1], method, sum, args.tail)
+        #print the results of the verification
         print("These results were generated using the sum of", method_string, "must be less than", sum)
-        print("{:<20} {:<30}".format("Height", "Zeros needed to verify RH"), end='')
+        result_strings = ["Height", "Zeros needed to verify RH"]
         if args.tail:
-            print("Tail Sum")
-        else:
-            print()
+            result_strings.append("Tail Sum")
+        for word in result_strings:
+            print("{:<30}".format(word), end="")
+        print()
         for result in results:
-          print("{:<20} {:<30}".format(result[0], result[1]), end='')
-          if args.tail:
-            print(result[2])
-        else:
+            for number in result:
+                print("{:<30}".format(number), end="")
             print()
+        print()
 
 
 
